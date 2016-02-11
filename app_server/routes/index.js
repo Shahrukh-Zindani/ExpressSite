53befbfb8b704bfaa272ed755e02aca6c2b108ff
Shahrukh Zindani <szindani@nectarom.com>
2016-02-10 23:28:11 -0600

    Run one page successfully

---
+++ app_server/routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var ctrlmain= require('../controllers/main');
+
+/* GET home page. */
+router.get('/', ctrlmain.index);
+
+module.exports = router;
